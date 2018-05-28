6f386036b156155d388f986bd8388f9bc09fb1a1
Sara Carmichael <scarmichael@agnesirwin.org>
2018-05-27 21:39:12 -0400

    Update script.js

--- Week5/js/script.js
+++ Week5/js/script.js
@@ -13,7 +13,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtmlUrl = "../snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
